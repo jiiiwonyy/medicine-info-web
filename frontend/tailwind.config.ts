882d04d7afa4ef913fdd7c2f2483e4ecd18373a9
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-08-19 14:38:50 +0900

    chore: pretendard 폰트 설치 및 적용

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
