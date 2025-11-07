22e43e99095d3bd8ed70c01f15681595a6ccc87c
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-11-07 10:42:07 +0900

    feat: 보건의료법률 링크 업데이트

--- frontend/src/Pages/LawInfo.tsx
+++ frontend/src/Pages/LawInfo.tsx
@@ -5,52 +5,62 @@ export default function LawInfoPage() {
     {
       title: '간호법',
       link: 'https://www.law.go.kr/법령/간호법',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW//lsInfoP.do?lsiSeq=265413&lsId=014736&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '환자안전법',
-      link: 'https://www.law.go.kr/법령/환자안전법',
-      threeLink: '',
+      link: 'https://www.law.go.kr/LSW/lsSc.do?section=&menuId=1&subMenuId=15&tabMenuId=81&eventGubun=060101&query=%ED%99%98%EC%9E%90%EC%95%88%EC%A0%84%EB%B2%95#undefined',
+      threeLink:
+        'https://www.law.go.kr/LSW/lsInfoP.do?lsiSeq=265481&lsId=012242&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '의료법',
       link: 'https://www.law.go.kr/법령/의료법',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW/lsInfoP.do?lsiSeq=269965&lsId=001788&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '약사법',
       link: 'https://www.law.go.kr/법령/약사법',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW/lsInfoP.do?lsiSeq=217283&lsId=001783&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '의료기기법',
       link: 'https://www.law.go.kr/법령/의료기기법',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW/lsInfoP.do?lsiSeq=268885&lsId=009514&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '마약류 관리에 관한 법률',
       link: 'https://www.law.go.kr/법령/마약류관리에관한법률',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW//lsInfoP.do?lsiSeq=270395&lsId=002025&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '보건의료기본법',
       link: 'https://www.law.go.kr/법령/보건의료기본법',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW//lsInfoP.do?lsiSeq=267417&lsId=002029&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '지역보건법',
       link: 'https://www.law.go.kr/법령/지역보건법',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW//lsInfoP.do?lsiSeq=265479&lsId=000236&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '감염병의 예방 및 관리에 관한 법률',
       link: 'https://www.law.go.kr/법령/감염병의예방및관리에관한법률',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW//lsInfoP.do?lsiSeq=270385&lsId=001792&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
     {
       title: '응급의료에 관한 법률',
       link: 'https://www.law.go.kr/법령/응급의료에관한법률',
-      threeLink: '',
+      threeLink:
+        'https://www.law.go.kr/LSW//lsInfoP.do?lsiSeq=254067&lsId=000218&chrClsCd=010202&urlMode=lsEfInfoR&viewCls=thdCmpNewScP&ancYnChk=0#',
     },
   ];
 
