222d4cf682991efb2f7154e04477a459e07c5ae2
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-12-03 10:52:53 +0900

    fix: cors 헤더 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,11 +42,7 @@ app.add_middleware(
     allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=[
-        "Content-Type",
-        "X-Admin-Token",
-        "Authorization",
-    ],
+    allow_headers=["Content-Type", "Authorization", "X-Admin-Token", "Origin"],
 )
 
 
