362a4b388323864bc9eeb7d22c9d9bddc076b717
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2026-02-03 22:48:44 +0900

    chore: build 에러 수정

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
-/// <reference types="vitest/globals" />
+/// <reference types="vite/client" />
 /// <reference types="vitest/globals" />
 /// <reference types="@testing-library/jest-dom" />
